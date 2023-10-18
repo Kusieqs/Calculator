93df2e6fe599748ae3e16676d54de6eed8979cf7
Kusieqs <130232383+Kusieqs@users.noreply.github.com>
2023-10-18 19:49:14 +0200

    Logic: Clear Method / Plus Method / Writing numbers Design: Better colors, Added new TextBlock (old output)

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,10 +20,129 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        string oldOutput = "";
+        string output = "";
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void NumberMethod(object sender, RoutedEventArgs e)
+        {
+            
+            string name = ((Button)sender).Name;
+            if (output.Length < 8)
+            {
+                switch (name)
+                {
+                    case "One":
+                        output += "1";
+                        OutputTextBlock.Text = output;
+                        break;
+                    case "Two":
+                        output += "2";
+                        OutputTextBlock.Text = output;
+                        break;
+                    case "Three":
+                        output += "3";
+                        OutputTextBlock.Text = output;
+                        break;
+                    case "Four":
+                        output += "4";
+                        OutputTextBlock.Text = output;
+                        break;
+                    case "Five":
+                        output += "5";
+                        OutputTextBlock.Text = output;
+                        break;
+                    case "Six":
+                        output += "6";
+                        OutputTextBlock.Text = output;
+                        break;
+                    case "Seven":
+                        output += "7";
+                        OutputTextBlock.Text = output;
+                        break;
+                    case "Eight":
+                        output += "8";
+                        OutputTextBlock.Text = output;
+                        break;
+                    case "Nine":
+                        output += "9";
+                        OutputTextBlock.Text = output;
+                        break;
+                    case "Zero":
+                        output += "0";
+                        OutputTextBlock.Text = output;
+                        break;
+
+
+                }
+
+            }
+        }
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            oldOutput = "";
+            output = "";
+            OutputTextBlock.Text = "0";
+            TextToRead.Text = oldOutput;
+        }
+        private void Plus(object sender, RoutedEventArgs e)
+        {
+            if(oldOutput == "")
+            {
+                oldOutput = output + " +";
+                TextToRead.Text = oldOutput;
+                output = "";
+                OutputTextBlock.Text = "0";
+            }
+            else
+            {
+                string[] numbers = oldOutput.Split(' ');
+                double oldData = double.Parse(numbers[0]);
+                double newData = double.Parse(output);
+                oldOutput = (oldData+newData).ToString() + " +";
+                TextToRead.Text = oldOutput;
+                output = "";
+                OutputTextBlock.Text = "0";
+            }
+        }
+        private void Minus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Element(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MinusOrPlus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Division(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Multiplication(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+        private void Equal(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Comma(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
